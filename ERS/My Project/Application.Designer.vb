ece8506439583971d9199689149dd3dda748a07b
tolits123 <junjun_leonardo@yahoo.com>
2017-04-05 03:13:58 -0700

    Merge remote-tracking branch 'refs/remotes/origin/master' into ashlrem/master
    # Conflicts:
    #	ERS.suo
    #	ERS/AddClass.Designer.vb
    #	ERS/Module1.vb

--- ERS/My Project/Application.Designer.vb
+++ ERS/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.ERS.AdminCreate
+            Me.MainForm = Global.ERS.AddClass
         End Sub
     End Class
 End Namespace
